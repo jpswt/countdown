0a014648ba2e4a905cccdaa21da6b1103bb0b2a7
jpswt <joel_swt@yahoo.com>
2022-11-22 16:22:00 -0600

    works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
+const useCountDown = (onDone, initialTime) => {
+	const [, render] = useState(0);
+	const timeLeftRef = useRef(initialTime);
+	const intervalRef = useRef();
+
+	const start = () => {
+		intervalRef.current = setInterval(() => {
+			const newPrevTime = timeLeftRef.current - 1;
+			if (newPrevTime <= 0) {
+				onDone();
+				clearInterval(intervalRef.current);
+				timeLeftRef.current = 0;
+			} else {
+				timeLeftRef.current -= 1;
+			}
+			render(Symbol());
+		}, 1000);
+		render();
+	};
+
+	useEffect(() => {
+		start();
+		return () => {
+			clearInterval(intervalRef.current);
+		};
+	}, []);
+
+	return {
+		secondsLeft: timeLeftRef.current,
+		reset: () => {
+			timeLeftRef.current = initialTime;
+			start();
+		},
+	};
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { secondsLeft, reset } = useCountDown(() => console.log('done'), 5);
+
+	return (
+		<div className="App">
+			{secondsLeft}
+			<button onClick={reset}>Reset</button>
+		</div>
+	);
 }
 
 export default App;
